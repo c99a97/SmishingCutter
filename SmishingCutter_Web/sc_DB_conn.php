c617e5c99dc04800cc53b45fbb0fd8a4ee6ab8d2
c99a97 <71994876+c99a97@users.noreply.github.com>
2020-12-13 04:59:40 +0900

    Update sc_DB_conn.php

--- SmishingCutter_Web/sc_DB_conn.php
+++ SmishingCutter_Web/sc_DB_conn.php
@@ -1,5 +1,5 @@
 ﻿<?php
-	$db_url = "database-c99a97.cf6z54xvsafa.ap-northeast-2.rds.amazonaws.com";
+	$db_url = " **address mosaic** ";
 	$db_id = "cswin";
 	$db_pw = "cswin123!";
 	$db_name = "SC_c99a97";
